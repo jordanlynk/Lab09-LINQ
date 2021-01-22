d65405d80bcf0165019b0bdef5530988a252fffb
Jordan Kidwell <jkid2023@gmail.com>
2021-01-21 16:30:23 -0800

    Added in my classes and got correct packages installed.

---
+++ Linq-In-Manhattan/Classes/Geometry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linq_In_Manhattan
+{
+    class Geometry
+    {
+    }
+}
---
+++ Linq-In-Manhattan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Linq_In_Manhattan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
